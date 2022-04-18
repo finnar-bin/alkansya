214a85ef0ec3497e2f43d090f50c77a16e7cdf54
Nar Cuenca <narc.ph@gmail.com>
2022-04-18 21:22:55 +0800

    feat: render response

--- src/routes/view/[year]/[month].svelte
+++ src/routes/view/[year]/[month].svelte
@@ -21,6 +21,27 @@
     }
 </script>
 
+<script>
+    export let data = {};
+
+    console.log(data)
+</script>
+
 <section>
     This is the month preview page!
+
+    <p>Total Expenses: {data.totalExpenses}</p>
+    <p>Expenses:</p>
+    <ul>
+        {#each data.expenses as expense}
+            <li>
+                <ul>
+                    <li>Type: {expense.type}</li>
+                    <li>Amount: {expense.amount}</li>
+                    <li>Description: {expense.description}</li>
+                    <li>Date: {new Date(expense.date)}</li>
+                </ul>
+            </li>
+        {/each}
+    </ul>
 </section>
