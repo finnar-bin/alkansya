af8edd068f300b13d3907bef4922516f1b250a30
Nar Cuenca <lunar.jay.cuenca@benyameenissaoutlook.onmicrosoft.com>
2022-05-18 13:50:12 +0800

    chore: prevent multiple initiation of firebase-admin

--- src/lib/firebase/admin.js
+++ src/lib/firebase/admin.js
@@ -1,12 +1,16 @@
-import { initializeApp, cert } from 'firebase-admin/app';
+import { initializeApp, cert, getApps, getApp } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
+import { getFirestore } from 'firebase-admin/firestore';
 import * as credentials from './admin-credentials.json';
 
-const app = initializeApp(
-	{
-		credential: cert(credentials)
-	},
-	'ADMIN'
-);
+const app = getApps().length
+	? getApp('ADMIN')
+	: initializeApp(
+			{
+				credential: cert(credentials)
+			},
+			'ADMIN'
+	  );
 
 export const auth = getAuth(app);
+export const db = getFirestore(app);
