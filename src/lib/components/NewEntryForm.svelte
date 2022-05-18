532aa7ad027b8a360b6709052c467445de65f1c3
Nar Cuenca <lunar.jay.cuenca@benyameenissaoutlook.onmicrosoft.com>
2022-05-18 20:36:05 +0800

    feat(new-entry): moved to api call based submission

--- src/lib/components/NewEntryForm.svelte
+++ src/lib/components/NewEntryForm.svelte
@@ -1,30 +1,24 @@
 <script>
-	import {
-		setDoc,
-		doc,
-		getDoc,
-		updateDoc,
-		collection,
-		addDoc,
-		arrayUnion
-	} from 'firebase/firestore';
+	import { onMount } from 'svelte';
 	import { INCOME_TYPES, EXPENSE_TYPES, MONTHS, YEARS } from '$lib/config/constants';
-	import { db } from '$lib/firebase/client';
+	import user from '$lib/stores/user';
+
+	onMount(() => user.useLocalStorage());
 
 	/* Properties */
 	let newEntry = {
 		amount: '',
-		creator: 'Nar',
+		creator: $user.displayName,
 		timestamp: new Date().toISOString(),
 		description: '',
 		type: '',
 		year: '',
 		month: ''
 	};
-
 	let isOtherType = false;
 	let transactionType = '';
 	let types = [];
+	let isLoading = false;
 
 	/**
 	 * Checks if the type selected is others to show/hide
@@ -37,54 +31,35 @@
 	/**
 	 * Submits the new entry to the database.
 	 */
-	async function handleFormSubmit(type) {
-		console.log(newEntry);
-		// TODO: Replace creator with user reference
-		// TODO: Migrate to api call
+	async function handleFormSubmit() {
+		isLoading = true;
+
 		const data = {
 			amount: newEntry.amount,
 			creator: newEntry.creator,
 			description: newEntry.description,
 			timestamp: newEntry.timestamp,
-			type: newEntry.type
+			type: newEntry.type,
+			year: newEntry.year,
+			month: newEntry.month,
+			transactionType
 		};
 
-		console.log(data);
-		const year = newEntry.year.toString();
-		const month = newEntry.month.toString();
-		const monthRef = doc(db, year, month);
-		const monthSnap = await getDoc(monthRef);
+		const newEntryResponse = await fetch('/api/entry', {
+			method: 'POST',
+			headers: new Headers({ 'content-type': 'application/json' }),
+			body: JSON.stringify(data)
+		});
 
-		if (!monthSnap.exists()) {
-			const recordRef = doc(db, 'records', year);
-			const recordSnap = await getDoc(recordRef);
+		if (newEntryResponse.ok) {
+			handleDiscardChanges();
 
-			// If year already exists in records collection,
-			// add the month. Otherwise, create the new document
-			// for that year.
-			if (recordSnap.exists()) {
-				await updateDoc(recordRef, {
-					months: arrayUnion(month)
-				});
-			} else {
-				await setDoc(recordRef, {
-					months: [month]
-				});
-			}
+			// TODO: Show success message
+		} else {
+			// TODO: Show error message
 		}
 
-		const setUpdates = await setDoc(monthRef, {
-			lastUpdated: newEntry.timestamp,
-			updatedBy: newEntry.creator
-		});
-
-		const addEntry = await addDoc(collection(monthRef, type), data);
-
-		Promise.all([setUpdates, addEntry]).then(() => {
-			console.log('success!');
-
-			handleDiscardChanges();
-		});
+		isLoading = false;
 	}
 
 	/**
@@ -92,10 +67,9 @@
 	 * new entry form.
 	 */
 	function handleDiscardChanges() {
-		console.log('discard triggered');
 		newEntry = {
 			amount: '',
-			creator: 'Nar',
+			creator: $user.displayName,
 			timestamp: new Date().toISOString(),
 			description: '',
 			type: '',
@@ -145,6 +119,7 @@
 			name="transactionType"
 			value="income"
 			on:change={handleTransactionType}
+			disabled={isLoading}
 		/>
 		<label for="income">Income</label>
 		<input
@@ -153,6 +128,7 @@
 			name="transactionType"
 			value="expense"
 			on:change={handleTransactionType}
+			disabled={isLoading}
 		/>
 		<label for="expense">Expense</label>
 	</label>
@@ -163,7 +139,13 @@
 		<form on:submit|preventDefault={handleFormSubmit(transactionType)}>
 			<label>
 				Type:
-				<select bind:value={newEntry.type} on:change={checkType} name="type" required>
+				<select
+					bind:value={newEntry.type}
+					on:change={checkType}
+					name="type"
+					required
+					disabled={isLoading}
+				>
 					<option value selected disabled hidden>Select a type...</option>
 					{#each types as type}
 						<option value={type.value}>{type.name}</option>
@@ -179,6 +161,7 @@
 						placeholder="Description..."
 						bind:value={newEntry.description}
 						required
+						disabled={isLoading}
 					/>
 				</label>
 			{/if}
@@ -190,11 +173,12 @@
 					bind:value={newEntry.amount}
 					placeholder="1000"
 					required
+					disabled={isLoading}
 				/>
 			</label>
 			<label>
 				Year:
-				<select bind:value={newEntry.year} name="year" required>
+				<select bind:value={newEntry.year} name="year" required disabled={isLoading}>
 					<option value selected disabled hidden>Select a year...</option>
 					{#each YEARS as year}
 						<option value={year}>{year}</option>
@@ -203,7 +187,7 @@
 			</label>
 			<label>
 				Month:
-				<select bind:value={newEntry.month} name="month" required>
+				<select bind:value={newEntry.month} name="month" required disabled={isLoading}>
 					<option value selected disabled hidden>Select a month...</option>
 					{#each MONTHS as month, index}
 						<option value={index + 1}>{month}</option>
@@ -211,8 +195,10 @@
 				</select>
 			</label>
 			<div>
-				<button type="submit">Submit</button>
-				<button type="button" on:click={handleDiscardChanges}>Discard</button>
+				<button type="submit" disabled={isLoading}>Submit</button>
+				<button type="button" on:click={handleDiscardChanges} disabled={isLoading}
+					>Discard</button
+				>
 			</div>
 		</form>
 	{/if}
--- src/routes/api/entry.js
+++ src/routes/api/entry.js
@@ -2,6 +2,39 @@ import { db } from '$lib/firebase/admin';
 import { returnHttpError } from '$lib/utils';
 
 /**
+ * API call to add a new entry.
+ * @param {Object} request Request object.
+ * @returns {Object} Response.
+ */
+export const post = async ({ request }) => {
+	const { amount, creator, description, timestamp, type, year, month, transactionType } =
+		await request.json();
+	const _year = year.toString();
+	const _month = month.toString();
+	const monthRef = db.doc(`${_year}/${_month}`);
+	monthRef.set({
+		lastUpdated: timestamp,
+		updatedBy: creator
+	});
+
+	return monthRef
+		.collection(transactionType)
+		.add({
+			amount,
+			creator,
+			description,
+			timestamp,
+			type
+		})
+		.then(() => {
+			return {
+				status: 200
+			};
+		})
+		.catch((error) => returnHttpError(500, error));
+};
+
+/**
  * API call to delete an entry.
  * @param {Object} request Request object.
  * @returns {Object} Response.
@@ -14,6 +47,8 @@ export async function del({ request }) {
 	} = await request.json();
 	const entryRef = db.doc(`${year}/${month}/${type}/${id}`);
 
+	// TODO: Update last update in the document
+
 	return entryRef
 		.delete({ exists: true })
 		.then(() => {
@@ -21,7 +56,5 @@ export async function del({ request }) {
 				status: 200
 			};
 		})
-		.catch((error) => {
-			return returnHttpError(500, error);
-		});
+		.catch((error) => returnHttpError(500, error));
 }
