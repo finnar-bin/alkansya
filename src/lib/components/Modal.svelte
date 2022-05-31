5b2afc0873e0b80590bb28f27290b99b83671d20
Nar Cuenca <narc.ph@gmail.com>
2022-05-31 21:07:30 +0800

    feat: wip created reusable modal

---
+++ src/lib/components/Modal.svelte
@@ -0,0 +1,37 @@
+<script>
+	import Card from '$lib/components/Card.svelte';
+
+	export let isOpen = false;
+
+	/**
+	 * Closes the modal.
+	 * @param e {Object} Event object
+	 */
+	const handleClose = (e) => {
+		const { id } = e.target;
+
+		if (id === 'overlayBackground') {
+			isOpen = false;
+		}
+	};
+</script>
+
+{#if isOpen}
+	<section
+		id="overlayBackground"
+		class="fixed 
+		inset-0 
+		bg-black/90 
+		z-20 
+		grid 
+		place-content-center"
+		on:click={handleClose}
+	>
+		<Card>
+			<h1 slot="card-header">test</h1>
+			<div slot="card-body">
+				<slot />
+			</div>
+		</Card>
+	</section>
+{/if}
--- src/lib/components/NewEntryForm.svelte
+++ src/lib/components/NewEntryForm.svelte
@@ -3,6 +3,7 @@
 	import { INCOME_TYPES, EXPENSE_TYPES, MONTHS, YEARS } from '$lib/config/constants';
 	import user from '$lib/stores/user';
 	import Plus from '$lib/assets/Plus.svelte';
+	import Modal from '$lib/components/Modal.svelte';
 
 	const dispatch = createEventDispatcher();
 	onMount(() => user.useLocalStorage());
@@ -21,6 +22,7 @@
 	let transactionType = '';
 	let types = [];
 	let isLoading = false;
+	let isModalOpen = false;
 
 	/**
 	 * Checks if the type selected is others to show/hide
@@ -97,6 +99,13 @@
 
 		handleDiscardChanges();
 	}
+
+	/**
+	 * Opens the new entry modal.
+	 */
+	const handleOpenModal = () => {
+		isModalOpen = true;
+	};
 </script>
 
 <style>
@@ -111,101 +120,121 @@
 	}
 </style>
 
-<section>
-	<h1>Add a new transaction</h1>
-	<label>
-		Transaction Type:
-		<input
-			id="income"
-			type="radio"
-			name="transactionType"
-			value="income"
-			on:change={handleTransactionType}
-			disabled={isLoading}
-		/>
-		<label for="income">Income</label>
-		<input
-			id="expense"
-			type="radio"
-			name="transactionType"
-			value="expense"
-			on:change={handleTransactionType}
-			disabled={isLoading}
-		/>
-		<label for="expense">Expense</label>
-	</label>
+<!-- TODO: Fix the styling and mobile layout -->
+<Modal bind:isOpen={isModalOpen}>
+	<section>
+		<h1>Add a new transaction</h1>
+		<label>
+			Transaction Type:
+			<input
+				id="income"
+				type="radio"
+				name="transactionType"
+				value="income"
+				on:change={handleTransactionType}
+				disabled={isLoading}
+			/>
+			<label for="income">Income</label>
+			<input
+				id="expense"
+				type="radio"
+				name="transactionType"
+				value="expense"
+				on:change={handleTransactionType}
+				disabled={isLoading}
+			/>
+			<label for="expense">Expense</label>
+		</label>
 
-	{#if transactionType === ''}
-		<p>Please select a transaction type...</p>
-	{:else}
-		<form on:submit|preventDefault={handleFormSubmit(transactionType)}>
-			<label>
-				Type:
-				<select
-					bind:value={newEntry.type}
-					on:change={checkType}
-					name="type"
-					required
-					disabled={isLoading}
-				>
-					<option value selected disabled hidden>Select a type...</option>
-					{#each types as type}
-						<option value={type.value}>{type.name}</option>
-					{/each}
-				</select>
-			</label>
-			{#if isOtherType}
+		{#if transactionType === ''}
+			<p>Please select a transaction type...</p>
+		{:else}
+			<form on:submit|preventDefault={handleFormSubmit(transactionType)}>
 				<label>
-					Description:
+					Type:
+					<select
+						bind:value={newEntry.type}
+						on:change={checkType}
+						name="type"
+						required
+						disabled={isLoading}
+					>
+						<option value selected disabled hidden>Select a type...</option>
+						{#each types as type}
+							<option value={type.value}>{type.name}</option>
+						{/each}
+					</select>
+				</label>
+				{#if isOtherType}
+					<label>
+						Description:
+						<input
+							type="text"
+							name="otherDescription"
+							placeholder="Description..."
+							bind:value={newEntry.description}
+							required
+							disabled={isLoading}
+						/>
+					</label>
+				{/if}
+				<label>
+					Amount:
 					<input
-						type="text"
-						name="otherDescription"
-						placeholder="Description..."
-						bind:value={newEntry.description}
+						type="number"
+						name="amount"
+						bind:value={newEntry.amount}
+						placeholder="1000"
 						required
 						disabled={isLoading}
 					/>
 				</label>
-			{/if}
-			<label>
-				Amount:
-				<input
-					type="number"
-					name="amount"
-					bind:value={newEntry.amount}
-					placeholder="1000"
-					required
-					disabled={isLoading}
-				/>
-			</label>
-			<label>
-				Year:
-				<select bind:value={newEntry.year} name="year" required disabled={isLoading}>
-					<option value selected disabled hidden>Select a year...</option>
-					{#each YEARS as year}
-						<option value={year}>{year}</option>
-					{/each}
-				</select>
-			</label>
-			<label>
-				Month:
-				<select bind:value={newEntry.month} name="month" required disabled={isLoading}>
-					<option value selected disabled hidden>Select a month...</option>
-					{#each MONTHS as month, index}
-						<option value={index + 1}>{month}</option>
-					{/each}
-				</select>
-			</label>
-			<div>
-				<button type="submit" disabled={isLoading}>Submit</button>
-				<button type="button" on:click={handleDiscardChanges} disabled={isLoading}
-					>Discard</button
-				>
-			</div>
-		</form>
-	{/if}
-</section>
+				<label>
+					Year:
+					<select bind:value={newEntry.year} name="year" required disabled={isLoading}>
+						<option value selected disabled hidden>Select a year...</option>
+						{#each YEARS as year}
+							<option value={year}>{year}</option>
+						{/each}
+					</select>
+				</label>
+				<label>
+					Month:
+					<select bind:value={newEntry.month} name="month" required disabled={isLoading}>
+						<option value selected disabled hidden>Select a month...</option>
+						{#each MONTHS as month, index}
+							<option value={index + 1}>{month}</option>
+						{/each}
+					</select>
+				</label>
+				<div>
+					<button type="submit" disabled={isLoading}>Submit</button>
+					<button type="button" on:click={handleDiscardChanges} disabled={isLoading}
+						>Discard</button
+					>
+				</div>
+			</form>
+		{/if}
+	</section>
+</Modal>
 
-<button class="btn-primary rounded-full fixed bottom-4 right-4 md:right-8 xl:right-36 2xl:right-40">
-	<Plus />
-</button>
+<div
+	class="fixed inset-x-0 bottom-0 pointer-events-none flex justify-end p-4 md:px-8 md:py-6 xl:px-36 2xl:px-40"
+>
+	<!-- Button for mobile -->
+	<button
+		class="btn-primary flex rounded-full pointer-events-auto md:hidden"
+		on:click={handleOpenModal}
+	>
+		<Plus />
+	</button>
+
+	<!-- Button for desktop -->
+	<button
+		class="btn-primary hidden items-center pointer-events-auto md:flex"
+		on:click={handleOpenModal}
+	>
+		<Plus customClass="inline mr-2" />
+		<span>Add New Entry</span>
+	</button>
+</div>
