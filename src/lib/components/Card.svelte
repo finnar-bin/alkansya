5d1af033ad4b0f4892a8de0550f7db7d62ada427
Nar Cuenca <narc.ph@gmail.com>
2022-06-02 13:43:15 +0800

    fix: fixed card border radiuses

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -1,22 +1,8 @@
-<style lang="postcss">
-	.card {
-		@apply rounded shadow-lg shadow-black/20 max-w-xs overflow-auto sm:max-w-sm md:max-w-2xl;
-	}
-
-	.card .header {
-		@apply bg-gray-600 p-6;
-	}
-
-	.card .body {
-		@apply bg-gray-700 p-6;
-	}
-</style>
-
-<div class="card">
-	<div class="header">
+<div class="shadow-lg shadow-black/20 container">
+	<div class="bg-gray-600 rounded-t p-6">
 		<slot name="card-header" />
 	</div>
-	<div class="body">
+	<div class="bg-gray-700 rounded-b p-6">
 		<slot name="card-body" />
 	</div>
 </div>
