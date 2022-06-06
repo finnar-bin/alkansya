97d414d3c4bff6524f07876eecad005a6b4fa7f2
Nar Cuenca <narc.ph@gmail.com>
2022-06-06 11:12:09 +0800

    feat: streamlined data refresh indicator in month view

--- src/routes/view/[year]/[month].svelte
+++ src/routes/view/[year]/[month].svelte
@@ -75,20 +75,13 @@
 	import Entry from '$lib/components/Entry.svelte';
 	import Dollar from '$lib/assets/Dollar.svelte';
 	import ShoppingCart from '$lib/assets/ShoppingCart.svelte';
+	import Loading from '$lib/assets/Loading.svelte';
+	import Notification from '$lib/components/Notification.svelte';
 
 	/** Properties */
 	export let monthData = {};
 	export let month, year;
-	export let incomes = [];
-	export let expenses = [];
-	export let totalExpenses = 0;
-	export let totalIncome = 0;
-	export let updateData = {
-		updatedBy: '',
-		lastUpdated: ''
-	};
 	$: pageHeader = `${getMonthString(month)} ${year}`;
-	$: monthData, setupMonthData(monthData);
 
 	/**
 	 * Sends an api call to delete an entry.
@@ -112,7 +105,7 @@
 		});
 
 		if (delResponse.ok) {
-			refreshEntries();
+			handleRefreshEntries();
 		}
 	}
 
@@ -138,7 +131,7 @@
 		});
 
 		if (updateResponse.ok) {
-			refreshEntries();
+			handleRefreshEntries();
 		}
 	};
 
@@ -149,25 +142,18 @@
 	const refreshEntries = async () => {
 		const monthEntriesResponse = await getEntries(fetch, { year, month });
 
-		console.log(monthEntriesResponse);
-
 		if (monthEntriesResponse.status === 200) {
-			monthData = monthEntriesResponse.data;
+			return monthEntriesResponse.data;
 		} else {
 			throw new Error(monthEntriesResponse.data);
 		}
 	};
 
 	/**
-	 * Sets raw month data to each property accordingly.
-	 * @param data
+	 * Triggers a data refresh
 	 */
-	const setupMonthData = (data) => {
-		incomes = data.incomes;
-		expenses = data.expenses;
-		totalExpenses = data.totalExpenses;
-		totalIncome = data.totalIncome;
-		updateData = data.updateData;
+	const handleRefreshEntries = () => {
+		monthData = refreshEntries();
 	};
 </script>
 
@@ -179,66 +165,75 @@
 	<a href="/">Back</a>
 	<h1 class="text-2xl border-b-2 pb-2">{pageHeader}</h1>
 
-	<div class="py-6">
-		<div>
-			<span class="font-black">Total Expenses: </span>
-			<span>{totalExpenses ? currencyFormat(totalExpenses) : '-'}</span>
+	{#await monthData}
+		<div class="py-6 flex items-center">
+			<span>Refreshing entries...</span>
+			<Loading customClass="animate-spin ml-2" />
 		</div>
-		<div>
-			<span class="font-black">Total Income: </span>
-			<span>{totalIncome ? currencyFormat(totalIncome) : '-'}</span>
+	{:then data}
+		<div class="py-6">
+			<div>
+				<span class="font-black">Total Expenses: </span>
+				<span>{data.totalExpenses ? currencyFormat(data.totalExpenses) : '-'}</span>
+			</div>
+			<div>
+				<span class="font-black">Total Income: </span>
+				<span>{data.totalIncome ? currencyFormat(data.totalIncome) : '-'}</span>
+			</div>
 		</div>
-	</div>
 
-	<div class="grid gap-8 grid-cols-1 md:grid-cols-2">
-		<Card noPadding>
-			<div class="text-2xl font-black" slot="card-header">
-				<Dollar customClass="mr-2 inline" /> Incomes
-			</div>
-			<div slot="card-body">
-				{#if incomes.length}
-					{#each incomes as income, index}
-						<Entry
-							type="income"
-							entry={income}
-							showBottomBorder={index + 1 < incomes.length}
-							on:delete-entry={deleteEntry('income', income.id)}
-							on:update-entry={(e) => updateEntry(e, 'income', income.id)}
-						/>
-					{/each}
-				{:else}
-					<p class="p-6">No data...</p>
-				{/if}
-			</div>
-		</Card>
+		<div class="grid gap-8 grid-cols-1 md:grid-cols-2">
+			<Card noPadding>
+				<div class="text-2xl font-black" slot="card-header">
+					<Dollar customClass="mr-2 inline" /> Incomes
+				</div>
+				<div slot="card-body">
+					{#if data.incomes.length}
+						{#each data.incomes as income, index}
+							<Entry
+								type="income"
+								entry={income}
+								showBottomBorder={index + 1 < data.incomes.length}
+								on:delete-entry={deleteEntry('income', income.id)}
+								on:update-entry={(e) => updateEntry(e, 'income', income.id)}
+							/>
+						{/each}
+					{:else}
+						<p class="p-6">No data...</p>
+					{/if}
+				</div>
+			</Card>
 
-		<Card noPadding>
-			<div class="text-2xl font-black" slot="card-header">
-				<ShoppingCart customClass="mr-2 inline" /> Expenses
-			</div>
-			<div slot="card-body">
-				{#if expenses.length}
-					{#each expenses as expense, index}
-						<Entry
-							type="expense"
-							entry={expense}
-							showBottomBorder={index + 1 < expenses.length}
-							on:delete-entry={deleteEntry('expense', expense.id)}
-							on:update-entry={(e) => updateEntry(e, 'expense', expense.id)}
-						/>
-					{/each}
-				{:else}
-					<p class="p-6">No data...</p>
-				{/if}
-			</div>
-		</Card>
-	</div>
+			<Card noPadding>
+				<div class="text-2xl font-black" slot="card-header">
+					<ShoppingCart customClass="mr-2 inline" /> Expenses
+				</div>
+				<div slot="card-body">
+					{#if data.expenses.length}
+						{#each data.expenses as expense, index}
+							<Entry
+								type="expense"
+								entry={expense}
+								showBottomBorder={index + 1 < data.expenses.length}
+								on:delete-entry={deleteEntry('expense', expense.id)}
+								on:update-entry={(e) => updateEntry(e, 'expense', expense.id)}
+							/>
+						{/each}
+					{:else}
+						<p class="p-6">No data...</p>
+					{/if}
+				</div>
+			</Card>
+		</div>
 
-	<div class="my-8 border-t-2 text-base text-zinc-400">
-		<p class="mt-4">
-			<span class="font-black">Last updated:</span>
-			{dateFormat(updateData.lastUpdated, 'datetime')}
-		</p>
-		<p><span class="font-black">Updated by:</span> {updateData.updatedBy}</p>
-	</div>
+		<div class="my-8 border-t-2 text-base text-zinc-400">
+			<p class="mt-4">
+				<span class="font-black">Last updated:</span>
+				{dateFormat(data.updateData.lastUpdated, 'datetime')}
+			</p>
+			<p><span class="font-black">Updated by:</span> {data.updateData.updatedBy}</p>
+		</div>
+	{:catch error}
+		<Notification type="error">{error.message}</Notification>
+	{/await}
 </section>
