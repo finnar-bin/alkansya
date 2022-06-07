8bedf7e8dedc5d03cf068191247ee2a0776c78db
Nar Cuenca <narc.ph@gmail.com>
2022-06-07 14:05:00 +0800

    ci: added different config for dev and prod

--- src/lib/firebase/admin.js
+++ src/lib/firebase/admin.js
@@ -1,11 +1,12 @@
 import { initializeApp, cert, getApps, getApp } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
-import * as adminCredentials from './admin-credentials.json';
+import getConfig from './admin-credentials';
+import { dev } from '$app/env';
 
 const app = getApps().length
 	? getApp('ADMIN')
-	: initializeApp({ credential: cert(adminCredentials) }, 'ADMIN');
+	: initializeApp({ credential: cert(getConfig(dev)) }, 'ADMIN');
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
