6995b9e73abb8132499c617158059beffa70d1b0
Nar Cuenca <narc.ph@gmail.com>
2022-06-07 13:31:32 +0800

    ci: changed to locally saved creds

--- src/lib/firebase/admin.js
+++ src/lib/firebase/admin.js
@@ -1,30 +1,7 @@
 import { initializeApp, cert, getApps, getApp } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const project_id = process.env['PROJECT_ID'];
-const private_key_id = process.env['PRIVATE_KEY_ID'];
-const private_key = process.env['PRIVATE_KEY'];
-const client_email = process.env['CLIENT_EMAIL'];
-const client_id = process.env['CLIENT_ID'];
-const client_x509_cert_url = process.env['CLIENT_X509_CERT_URL'];
-
-/* Firebase admin credentials */
-export const adminCredentials = {
-	type: 'service_account',
-	auth_uri: 'https://accounts.google.com/o/oauth2/auth',
-	token_uri: 'https://oauth2.googleapis.com/token',
-	auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-	client_x509_cert_url,
-	project_id,
-	private_key_id,
-	private_key,
-	client_email,
-	client_id
-};
+import * as adminCredentials from './admin-credentials.json';
 
 const app = getApps().length
 	? getApp('ADMIN')
