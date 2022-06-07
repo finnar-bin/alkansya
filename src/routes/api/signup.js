44fdc59a70fd141aeb2bb91ac9a732135e2112b2
Nar Cuenca <narc.ph@gmail.com>
2022-06-07 16:51:57 +0800

    chore: remove token creation on signup

--- src/routes/api/signup.js
+++ src/routes/api/signup.js
@@ -1,8 +1,5 @@
 import { auth } from '$lib/firebase/admin';
 import { returnHttpError } from '$lib/utils';
-import { dev } from '$app/env';
-
-const secure = dev ? '' : 'Secure;';
 
 /**
  * API to register a new user.
@@ -19,26 +16,13 @@ export const post = async ({ request }) => {
 			displayName: data.displayName
 		});
 
-		try {
-			const token = await auth.createCustomToken(userRecord.uid);
-
-			return {
-				status: 200,
-				headers: {
-					'content-type': 'application/json',
-					'set-cookie': `customToken=${token}; Max-Age=${
-						60 * 55
-					}; Path=/; HttpOnly; ${secure}`,
-					'cache-control': 'no-store'
-				},
-				body: JSON.stringify({
-					email: userRecord.email,
-					token
-				})
-			};
-		} catch (error) {
-			return returnHttpError(500, error.message);
-		}
+		return {
+			status: 200,
+			headers: new Headers({ 'content-type': 'application/json' }),
+			body: JSON.stringify({
+				email: userRecord.email
+			})
+		};
 	} catch (error) {
 		return returnHttpError(500, error.message);
 	}
